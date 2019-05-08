f9f78d761861d2617216373e0c208f0543039e97
Shahab Khalvati <contact@shahabkhalvati.com>
2019-05-09 02:36:01 +0300

    support for transactions

--- db/pg.js
+++ db/pg.js
@@ -11,5 +11,21 @@ const pool = new pg.Pool({
 module.exports = {
   query: (text, params, callback) => {
     return pool.query(text, params, callback)
+  },
+  inTransaction: async function (callback) {
+    const client = await pool.connect()
+
+    try {
+      await client.query('BEGIN')
+      const finalResult = await callback(client)
+      await client.query('COMMIT')
+
+      return finalResult
+    } catch (e) {
+      await client.query('ROLLBACK')
+      throw e
+    } finally {
+      client.release()
+    }
   }
 }
